c3382e35cf5bd7027205f5c258bd02b260401978
aiegoo@gmail.com <eozz21@gmail.com>
2022-08-01 01:40:22 +0900

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Archery Kinect Game',
   tagline: 'TEAMC',
-  url: 'https://aiegoo.github.io/archery',
+  url: 'https://aiegoo.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
