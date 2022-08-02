844a60e6cbc61fb7cfd1c477c901ac70124a96e5
aiegoo@gmail.com <eozz21@gmail.com>
2022-08-02 23:16:41 +0900

    update sidebar

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,7 +33,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebarArchery.js'),
+          sidebarPath: require.resolve('./sidebar.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
