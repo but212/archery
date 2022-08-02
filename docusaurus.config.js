96c12167369606d4d78bb44ee7734084e7f7984e
aiegoo@gmail.com <eozz21@gmail.com>
2022-08-02 18:15:57 +0900

    update footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -120,7 +120,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} KOCCA, Kidp and Joyfun collaboration program for TeamC.`,
       },
       prism: {
         theme: lightCodeTheme,
