9b3c975f6d9f321d41bc316235db89785f5b53a6
tonyleekorea <42961200+aiegoo@users.noreply.github.com>
2022-08-05 21:00:42 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/aiegoo/archery/edit/master/blog/',
+            'https://github.com/aiegoo/archery/edit/master/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
