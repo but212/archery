72f6e044e6193a12e4e349b979af6e03bacc10ec
aiegoo@gmail.com <eozz21@gmail.com>
2022-08-09 15:57:53 +0900

    update site title to leviathan

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Archery Kinect Game',
+        title: 'Leviathan Kinect Game',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
