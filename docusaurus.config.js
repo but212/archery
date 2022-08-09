4ba51dd8663c26ad7b263cbbcf787f021792622e
tonyleekorea <42961200+aiegoo@users.noreply.github.com>
2022-08-09 22:24:54 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,6 +42,9 @@ const config = {
         },
         blog: {
           showReadingTime: true,
+          blogSidebarCount: 10,
+          postsPerPage: 10,
+          blogSidebarTitle: 'All our posts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
